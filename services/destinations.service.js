4d4e40c4b8a7a6fc2a5a2867d01ebb84955e749d
Shashikant Chobhe <shashikant@isgesolutions.com>
2023-08-03 16:53:55 -0400

    worked on the service integration

--- services/destinations.service.js
+++ services/destinations.service.js
@@ -10,8 +10,11 @@ const baseUrl_dropdown = `${publicRuntimeConfig.apiUrl}/destinations_dropdown`;
 // const destinationLandingPageUrl = `${publicRuntimeConfig.apiUrl}/api/custom-pages?filters[custom-page][page_code][$eq]=Destinations&populate[0]=custom_page_images`;
 const destinationLandingPageUrl = `${publicRuntimeConfig.apiUrl}/destination_landing_page`;
 
-// const destinationLandingListUrl = `${publicRuntimeConfig.apiUrl}//api/destinations?fields[0]=destination_code&fields[1]=destination_name&populate[0]=destination_images;
+// const destinationLandingListUrl = `${publicRuntimeConfig.apiUrl}/api/destinations?fields[0]=destination_code&fields[1]=destination_name&populate[0]=destination_images;
 const destinationLandingListUrl = `${publicRuntimeConfig.apiUrl}/destination_landing_list`;
+
+// const destinationDetailsUrl = `${publicRuntimeConfig.apiUrl}/api/destinations/1?populate[0]=destination_images;
+const destinationDetailsUrl = `${publicRuntimeConfig.apiUrl}/destination_details`;
 
 const baseUrl = `${publicRuntimeConfig.apiUrl}/destinations`;
 
@@ -33,7 +36,8 @@ export const destinationService = {
     getById,
     getAllDropdown,
     getDestinationLandingPage,
-    getDestinationLandingList
+    getDestinationLandingList,
+    getDestinationDetails
 };
 
 function getAllDropdown() {
@@ -59,3 +63,9 @@ function getDestinationLandingList() {
     // console.log('baseUrl_dropdown', baseUrl_dropdown);
     return fetchWrapper.get(destinationLandingListUrl);
 }
+
+function getDestinationDetails() {
+    // console.log('baseUrl_dropdown', baseUrl_dropdown);
+    return fetchWrapper.get(destinationDetailsUrl);
+}
+
