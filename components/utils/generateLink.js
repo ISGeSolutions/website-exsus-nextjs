51f372b335f1ae018ebd07d8233b5062224aa1f5
Shashikant Chobhe <shashikant@isgesolutions.com>
2023-08-04 13:23:08 -0400

    worked on the link issues

---
+++ components/utils/generateLink.js
@@ -0,0 +1,10 @@
+// utils/generateLink.js
+const generateDynamicLink = (itemId) => {
+    // Replace this with your logic to generate the dynamic link URL based on itemId
+    // if(itemId && itemId == 'AF') {
+    //     return `/destination/africa`;
+    // }
+
+  };
+  
+  export default generateDynamicLink;
