56c2f0bc492f8ebe5cbeef19f2139ef2568b15a0
Shashikant Chobhe <shashikant@isgesolutions.com>
2023-08-10 20:31:25 -0400

    Worked on the dropdown

--- pages/uk/destinations/africa/index.jsx
+++ pages/uk/destinations/africa/index.jsx
@@ -5,6 +5,12 @@ import { userService, destinationService } from 'services';
 import Iframe from 'react-iframe'
 import Head from 'next/head';
 
+import React from 'react';
+import Select from 'react-select';
+// Import Bootstarp CSS
+// import "bootstrap/dist/css/bootstrap.css";
+// import { MultiSelect } from "react-multi-select-component";
+
 // import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 // var Carousel = require('react-responsive-carousel').Carousel;
 
@@ -18,23 +24,105 @@ function Index() {
     const [backgroundImage, setBackgroundImage] = useState('');
     const [valueWithBr, setnewValueWithBr] = useState('');
     const [headingText, setHeadingText] = useState('LUXURY SAFARI HOLIDAYS IN AFRICA');
+    const [selected, setSelected] = useState([]);
+
+    const [isClearable, setIsClearable] = useState(true);
+    const [isSearchable, setIsSearchable] = useState(true);
+    const [isDisabled, setIsDisabled] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isRtl, setIsRtl] = useState(false);
+
+    const [selectedOption, setSelectedOption] = useState(null);
+
+    const countryOptions = [
+        { value: "", label: "Filter by country" },
+        { value: "Asia", label: "Asia" },
+        { value: "Hong Kong & Macau", label: "Hong Kong & Macau" },
+        { value: "Malaysia & Borneo", label: "Malaysia & Borneo" },
+        { value: "Singapore", label: "Singapore" },
+        { value: "Indonesia", label: "Indonesia" },
+        { value: "Japan", label: "Japan" },
+        { value: "Cambodia", label: "Cambodia" },
+        { value: "Vietnam", label: "Vietnam" },
+        { value: "China", label: "China" },
+        { value: "Thailand", label: "Thailand" },
+        { value: "Burma", label: "Burma" },
+        { value: "Laos", label: "Laos" }
+    ];
+
+    const regionOptions = [
+        { value: "", label: "Filter by property type" },
+        { value: "Everything", label: "Everything" },
+        { value: "Barefoot", label: "Barefoot" },
+        { value: "Beach", label: "Beach" },
+        { value: "Boutique hotel", label: "Boutique hotel" },
+        { value: "Chic design", label: "Chic design" },
+        { value: "Cultural Immersion", label: "Cultural Immersion" },
+        { value: "Eco tourism", label: "Eco tourism" },
+        { value: "Family-Friendly", label: "Family-Friendly" },
+        { value: "Food & Wine", label: "Food & Wine" },
+        { value: "Guiding", label: "Guiding" },
+        { value: "Hideaway", label: "Hideaway" },
+        { value: "Honeymoon", label: "Honeymoon" },
+        { value: "Lodge", label: "Lodge" },
+        { value: "Luxury hotel", label: "Luxury Hotel" },
+        { value: "Off the beaten track", label: "Off the beaten track" },
+        { value: "Owner run", label: "Owner run" },
+        { value: "Peace & quiet", label: "Peace & quiet" },
+        { value: "Private groups", label: "Private groups" },
+        { value: "Romantic", label: "Romantic" },
+        { value: "Rustic", label: "Rustic" },
+        { value: "Seriously special", label: "Seriously special" },
+        { value: "Service & Hospitality", label: "Service & Hospitality" },
+        { value: "Setting & Views", label: "Setting & Views" },
+        { value: "Snorkelling & Driving", label: "Snorkelling & Driving" },
+        { value: "Spa & Wellness", label: "Spa & Wellness" },
+        { value: "Unusal", label: "Unusal" },
+        { value: "Village life", label: "Village life" },
+        { value: "Walking & trekking", label: "Walking & trekking" },
+        { value: "Water activities", label: "Water activities" },
+        { value: "Wildlife & Nature", label: "Wildlife & Nature" },
+        { value: "Adventure", label: "Adventure" },
+        { value: "Couples", label: "Couples" },
+        { value: "Educational", label: "Educational" },
+        { value: "Multi-activity", label: "Multi-activity" },
+        { value: "Teenagers", label: "Teenagers" },
+        { value: "Landscapes & Scenery", label: "Landscapes & Scenery" },
+        { value: "City hotel", label: "City hotel" }
+    ];
+
+    const monthOptions = [
+        { value: "", label: "Filter by month" },
+        { value: "All months", label: "All months" },
+        { value: "January", label: "January" },
+        { value: "February", label: "February" },
+        { value: "March", label: "March" },
+        { value: "April", label: "April" },
+        { value: "May", label: "May" },
+        { value: "June", label: "June" },
+        { value: "July", label: "July" },
+        { value: "August", label: "August" },
+        { value: "September", label: "September" },
+        { value: "October", label: "October" },
+        { value: "November", label: "November" },
+        { value: "December", label: "December" }
+    ];
 
     const selectedSec = (itemId) => {
         // console.log('itemId', itemId);
-
         var text = "LUXURY SAFARI HOLIDAYS IN AFRICA";
-        if(itemId == 'overview') {
+        if (itemId == 'overview') {
             text = "LUXURY SAFARI HOLIDAYS IN AFRICA";
-        } else if(itemId == 'countries') {
+        } else if (itemId == 'countries') {
             text = "COUNTRIES IN AFRICA";
-        } else if(itemId == 'itineraries') {
+        } else if (itemId == 'itineraries') {
             text = "TAILOR-MADE AFRICA HOLIDAY ITINERARIES";
-        } else if(itemId == 'places_to_stay') {
+        } else if (itemId == 'places_to_stay') {
             text = "PLACES TO STAY IN AFRICA";
         } else {
             text = "LUXURY SAFARI HOLIDAYS IN AFRICA";
         }
-        setHeadingText(text)
+        setHeadingText(text);
 
         // LUXURY SAFARI HOLIDAYS IN AFRICA
         // COUNTRIES IN AFRICA
@@ -44,7 +132,6 @@ function Index() {
     };
 
     useEffect(() => {
-
         userService.getAll().then(x => setUsers(x));
         const carousel = document.querySelector('#carouselExampleInterval');
         new bootstrap.Carousel(carousel);
@@ -68,7 +155,7 @@ function Index() {
             // const lines = x.data.attributes?.overview_text.split('\n');
             // console.log('lines', lines);
             const oldText = x.data.attributes?.overview_text;
-            var newValueWithBr = oldText?.replace(/\n/g,"<br />");
+            var newValueWithBr = oldText?.replace(/\n/g, "<br />");
             setnewValueWithBr(newValueWithBr);
             setBackgroundImage("https://d33ys3jnmuivbg.cloudfront.net/ilimages" + x.data.attributes.destination_images.data[0].attributes.image_path);
             // setDestinationLandingDetails(x)
@@ -102,8 +189,6 @@ function Index() {
                 <script type="text/javascript" src="/assets/javascripts/card-slider-equal-height.js"></script>
             </Head>
             <section className="banner_blk_row">
-
-
                 <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
                     <div className="carousel-indicators">
                         <button type="button" data-bs-target="#carouselExampleInterval" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
@@ -121,7 +206,6 @@ function Index() {
                         <button type="button" data-bs-target="#carouselExampleInterval" data-bs-slide-to="12" aria-label="Slide 13"></button>
                     </div>
                     <div className="carousel-inner">
-
                         <a href="#" target="_blank" className="carousel-item active" data-bs-interval="5000">
                             <div className="banner_commn_cls" style={{ backgroundImage: `url(${backgroundImage})` }}></div>
                         </a>
@@ -222,7 +306,7 @@ function Index() {
                     <div className="tab-pane fade show active" id="pills-overview" role="tabpanel" aria-labelledby="pills-overview-tab" tabIndex="0">
                         <div className="container-md">
                             <section className="destination_para">
-                            {/* {valueWithBr.map((line, index) => (
+                                {/* {valueWithBr.map((line, index) => (
         <p dangerouslySetInnerHTML={{ __html: line}} key={index} />
       ))} */}
                                 {<div dangerouslySetInnerHTML={{ __html: valueWithBr }} />}
@@ -543,7 +627,6 @@ function Index() {
                                 </div>
                             </div>
                         </section>
-
                     </div>
                     <div className="tab-pane fade" id="pills-countries" role="tabpanel" aria-labelledby="pills-countries-tab" tabIndex="0">
                         <div className="container-md">
@@ -881,6 +964,66 @@ function Index() {
                                         <div className="row">
                                             <div className="col-12">
                                                 <div className="destination_dropdwn_row d-block d-md-flex">
+                                                    <div class="d-flex justify-content-between">
+                                                        <div className="banner_dropdwn_blk">
+                                                            <Select
+                                                                placeholder="Select Portfolio"
+                                                                className="basic-single"
+                                                                classNamePrefix="select"
+                                                                defaultValue={countryOptions[0]}
+                                                                isDisabled={isDisabled}
+                                                                isLoading={isLoading}
+                                                                isClearable={isClearable}
+                                                                isRtl={isRtl}
+                                                                isSearchable={isSearchable}
+                                                                name="color"
+                                                                options={countryOptions}
+                                                                isMulti
+                                                            />
+                                                        </div>
+                                                        <div className="banner_dropdwn_blk ps-0 ps-md-2">
+                                                            <Select
+                                                                placeholder="Select Portfolio"
+                                                                className="basic-single"
+                                                                classNamePrefix="select"
+                                                                defaultValue={regionOptions[0]}
+                                                                isDisabled={isDisabled}
+                                                                isLoading={isLoading}
+                                                                isClearable={isClearable}
+                                                                isRtl={isRtl}
+                                                                isSearchable={isSearchable}
+                                                                name="color"
+                                                                options={regionOptions}
+                                                                isMulti
+                                                            />
+                                                        </div>
+                                                        <div className="banner_dropdwn_blk ps-0 ps-md-2">
+                                                            <Select
+                                                                placeholder="Select Portfolio"
+                                                                className="basic-single"
+                                                                classNamePrefix="select"
+                                                                defaultValue={monthOptions[0]}
+                                                                isDisabled={isDisabled}
+                                                                isLoading={isLoading}
+                                                                isClearable={isClearable}
+                                                                isRtl={isRtl}
+                                                                isSearchable={isSearchable}
+                                                                name="color"
+                                                                options={monthOptions}
+                                                                isMulti
+                                                            />
+                                                        </div>
+                                                        <div className="banner_dropdwn_blk ps-0 ps-md-2">
+                                                            <div className="banner_inspire_btn ps-0 ps-md-2">
+                                                                <button type="button" className="btn btn-primary prmry_btn">Inspire me
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" shapeRendering="geometricPrecision" textRendering="geometricPrecision" imageRendering="optimizeQuality" fillRule="evenodd" clipRule="evenodd" viewBox="0 0 267 512.43"><path fillRule="nonzero" d="M3.22 18.9c-4.28-4.3-4.3-11.31-.04-15.64s11.2-4.35 15.48-.04l245.12 245.16c4.28 4.3 4.3 11.31.04 15.64L18.66 509.22a10.874 10.874 0 0 1-15.48-.05c-4.26-4.33-4.24-11.33.04-15.63L240.5 256.22 3.22 18.9z"></path></svg>
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                {/* <div className="destination_dropdwn_row d-block d-md-flex">
                                                     <div className="banner_dropdwn_blk">
                                                         <div className="select_drpdwn">
                                                             <select className="selectpicker" multiple aria-label="Filter by country" data-live-search="true">
@@ -969,7 +1112,7 @@ function Index() {
                                                             <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" shapeRendering="geometricPrecision" textRendering="geometricPrecision" imageRendering="optimizeQuality" fillRule="evenodd" clipRule="evenodd" viewBox="0 0 267 512.43"><path fillRule="nonzero" d="M3.22 18.9c-4.28-4.3-4.3-11.31-.04-15.64s11.2-4.35 15.48-.04l245.12 245.16c4.28 4.3 4.3 11.31.04 15.64L18.66 509.22a10.874 10.874 0 0 1-15.48-.05c-4.26-4.33-4.24-11.33.04-15.63L240.5 256.22 3.22 18.9z"></path></svg>
                                                         </button>
                                                     </div>
-                                                </div>
+                                                </div> */}
                                             </div>
                                             <div className="col-12">
                                                 <div className="destination_filter_result d-block d-lg-flex">
