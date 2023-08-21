a56fce949f15f079b0d22fb9837ff30d0b588a41
GaneshDevkar <86193892+GaneshDevkar@users.noreply.github.com>
2023-08-21 20:17:32 +0530

    worked on dropdown changes in itenery

--- pages/asia/destinations/africa/index.jsx
+++ pages/asia/destinations/africa/index.jsx
@@ -1201,13 +1201,11 @@ function Index() {
                                                                 onChange={handleChange}
                                                             />
                                                         </div>
-                                                        <div className="banner_dropdwn_blk ps-0 ps-md-2">
-                                                            <div className="banner_inspire_btn ps-0 ps-md-2">
-                                                                <button type="button" className="btn btn-primary prmry_btn">Inspire me
-                                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" shapeRendering="geometricPrecision" textRendering="geometricPrecision" imageRendering="optimizeQuality" fillRule="evenodd" clipRule="evenodd" viewBox="0 0 267 512.43"><path fillRule="nonzero" d="M3.22 18.9c-4.28-4.3-4.3-11.31-.04-15.64s11.2-4.35 15.48-.04l245.12 245.16c4.28 4.3 4.3 11.31.04 15.64L18.66 509.22a10.874 10.874 0 0 1-15.48-.05c-4.26-4.33-4.24-11.33.04-15.63L240.5 256.22 3.22 18.9z"></path></svg>
-                                                                </button>
-                                                            </div>
-                                                        </div>
+                                                    </div>
+                                                    <div className="banner_inspire_btn ps-0 ps-md-2">
+                                                        <button type="button" className="btn btn-primary prmry_btn">Inspire me
+                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" shapeRendering="geometricPrecision" textRendering="geometricPrecision" imageRendering="optimizeQuality" fillRule="evenodd" clipRule="evenodd" viewBox="0 0 267 512.43"><path fillRule="nonzero" d="M3.22 18.9c-4.28-4.3-4.3-11.31-.04-15.64s11.2-4.35 15.48-.04l245.12 245.16c4.28 4.3 4.3 11.31.04 15.64L18.66 509.22a10.874 10.874 0 0 1-15.48-.05c-4.26-4.33-4.24-11.33.04-15.63L240.5 256.22 3.22 18.9z"></path></svg>
+                                                        </button>
                                                     </div>
                                                 </div>
 
