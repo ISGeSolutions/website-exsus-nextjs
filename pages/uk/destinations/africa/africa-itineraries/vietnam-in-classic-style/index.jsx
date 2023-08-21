19c0cd70a69d9ca5f8e207f461407fe48def54e4
Shashikant Chobhe <shashikant@isgesolutions.com>
2023-08-21 19:53:11 -0400

    Worke don the inline style

--- pages/uk/destinations/africa/africa-itineraries/vietnam-in-classic-style/index.jsx
+++ pages/uk/destinations/africa/africa-itineraries/vietnam-in-classic-style/index.jsx
@@ -137,8 +137,8 @@ function Index() {
                             <h3>All Exsus Travel holidays are tailor-made</h3>
                             <p>All itineraries on our website are designed as a starting point. Tell us your budget/wishlist/preferred length of stay and we'll help you select the best hotels and experiences, so your holiday is totally personalised.</p>
                             <div className="btn_grp">
-                                Call 020 7337 9010 or
-                                <button className="btn prmry_btn make_enqury_btn">Make an enquiry
+                                Call 020 7337 9010 or                                 
+                                <button className="btn prmry_btn make_enqury_btn ml-2"  style={{ marginLeft: '10px' }}>Make an enquiry
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" shapeRendering="geometricPrecision" textRendering="geometricPrecision" imageRendering="optimizeQuality" fillRule="evenodd" clipRule="evenodd" viewBox="0 0 267 512.43"><path fillRule="nonzero" d="M3.22 18.9c-4.28-4.3-4.3-11.31-.04-15.64s11.2-4.35 15.48-.04l245.12 245.16c4.28 4.3 4.3 11.31.04 15.64L18.66 509.22a10.874 10.874 0 0 1-15.48-.05c-4.26-4.33-4.24-11.33.04-15.63L240.5 256.22 3.22 18.9z" /></svg>
                                 </button>
                             </div>
