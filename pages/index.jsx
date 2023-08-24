6cd9ef03fd2504ec24c202e6c9414f89c6b87144
Shashikant Chobhe <shashikant@isgesolutions.com>
2023-08-24 13:48:34 -0400

    worked on the home page issue

--- pages/index.jsx
+++ pages/index.jsx
@@ -196,22 +196,21 @@ function Index() {
             <section className="card_blk_row">
                 <div className="container-md">
                     <div className="row">
-                        {/* <pre>{JSON.stringify(thumbnailImageArr, null, 2) } </pre> */}
                         {thumbnailImage?.map((holidaytypesItem, i) => (
                             <div className="col-sm-6 col-md-6 col-lg-4" key={i}>
                                 <div className="card_blk_inr">
-                                    <NavLink href={dynamicLink(holidaytypesItem?.holiday_type_code, holidaytypesItem?.id)} as={dynamicLinkHolidayas(holidaytypesItem?.attributes?.holiday_type_group_code, holidaytypesItem?.id)}>
-                                        <img src={dynamicThumbnailImage(holidaytypesItem.image_path)} alt="holiday_type01" className="img-fluid" />
+                                    <NavLink target="_blank" href={dynamicLink(holidaytypesItem?.holiday_type_code, holidaytypesItem?.id)} as={dynamicLinkHolidayas(holidaytypesItem?.attributes?.holiday_type_group_code, holidaytypesItem?.id)}>
+                                        <img src={dynamicThumbnailImage(holidaytypesItem.image_path)} alt="Card image 01" className="img-fluid" />
                                         <div className="card_blk_cntnt">
                                             <div className="row align-items-center">
                                                 <div className="col-11">
                                                     <div className="card_blk_txt">
-                                                        <h3>{holidaytypesItem?.holiday_type_name}</h3>
-                                                        <p></p>
+                                                        <h3>Bespoke Honeymoon ideas</h3>
+                                                        <p>The ultimate romantic escapes</p>
                                                     </div>
                                                 </div>
                                                 <div className="col-1 ps-0">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" shapeRendering="geometricPrecision" textRendering="geometricPrecision" imageRendering="optimizeQuality" fillRule="evenodd" clipRule="evenodd" viewBox="0 0 267 512.43"><path fillRule="nonzero" d="M3.22 18.9c-4.28-4.3-4.3-11.31-.04-15.64s11.2-4.35 15.48-.04l245.12 245.16c4.28 4.3 4.3 11.31.04 15.64L18.66 509.22a10.874 10.874 0 0 1-15.48-.05c-4.26-4.33-4.24-11.33.04-15.63L240.5 256.22 3.22 18.9z" /></svg>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 267 512.43"><path fill-rule="nonzero" d="M3.22 18.9c-4.28-4.3-4.3-11.31-.04-15.64s11.2-4.35 15.48-.04l245.12 245.16c4.28 4.3 4.3 11.31.04 15.64L18.66 509.22a10.874 10.874 0 0 1-15.48-.05c-4.26-4.33-4.24-11.33.04-15.63L240.5 256.22 3.22 18.9z" /></svg>
                                                 </div>
                                             </div>
                                         </div>
