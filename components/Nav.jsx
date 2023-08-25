5604eaa359fb755175a67e559213ddb0286e4ee2
Shashikant Chobhe <shashikant@isgesolutions.com>
2023-08-25 14:38:07 -0400

    worked on the menu

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -383,7 +383,7 @@ function Nav() {
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" shapeRendering="geometricPrecision" textRendering="geometricPrecision" imageRendering="optimizeQuality" fillRule="evenodd" clipRule="evenodd" viewBox="0 0 267 512.43"><path fillRule="nonzero" d="M3.22 18.9c-4.28-4.3-4.3-11.31-.04-15.64s11.2-4.35 15.48-.04l245.12 245.16c4.28 4.3 4.3 11.31.04 15.64L18.66 509.22a10.874 10.874 0 0 1-15.48-.05c-4.26-4.33-4.24-11.33.04-15.63L240.5 256.22 3.22 18.9z" /></svg>
                                     </NavLink> */}
                             {/* {overlayVisible && ( */}
-                                <div className="sub-menu mega-menu mega-menu-column-4" style={{ marginTop: '8px' }}>
+                                <div className="sub-menu mega-menu mega-menu-column-4">
                                     <div className="row">
                                         <div className="col-lg-6">
                                             <div className="row">
@@ -450,7 +450,7 @@ function Nav() {
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" shapeRendering="geometricPrecision" textRendering="geometricPrecision" imageRendering="optimizeQuality" fillRule="evenodd" clipRule="evenodd" viewBox="0 0 267 512.43"><path fillRule="nonzero" d="M3.22 18.9c-4.28-4.3-4.3-11.31-.04-15.64s11.2-4.35 15.48-.04l245.12 245.16c4.28 4.3 4.3 11.31.04 15.64L18.66 509.22a10.874 10.874 0 0 1-15.48-.05c-4.26-4.33-4.24-11.33.04-15.63L240.5 256.22 3.22 18.9z" /></svg>
                             </NavLink>
                             {/* {overlayVisible && ( */}
-                            <div className="sub-menu mega-menu mega-menu-column-4" style={{ marginTop: '8px' }}>
+                            <div className="sub-menu mega-menu mega-menu-column-4">
                                 <div className="row">
                                     <div className="col-lg-6">
                                         <div className="row">
@@ -714,7 +714,7 @@ function Nav() {
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" shapeRendering="geometricPrecision" textRendering="geometricPrecision" imageRendering="optimizeQuality" fillRule="evenodd" clipRule="evenodd" viewBox="0 0 267 512.43"><path fillRule="nonzero" d="M3.22 18.9c-4.28-4.3-4.3-11.31-.04-15.64s11.2-4.35 15.48-.04l245.12 245.16c4.28 4.3 4.3 11.31.04 15.64L18.66 509.22a10.874 10.874 0 0 1-15.48-.05c-4.26-4.33-4.24-11.33.04-15.63L240.5 256.22 3.22 18.9z" /></svg>
                             </NavLink>
                             {/* {overlayVisible && ( */}
-                            <div className="sub-menu mega-menu mega-menu-column-4" style={{ marginTop: '8px' }}>
+                            <div className="sub-menu mega-menu mega-menu-column-4">
                                 <div className="row">
                                     <div className="col-lg-6">
                                         <div className="row">
