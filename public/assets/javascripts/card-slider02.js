146dfc24e60ef1608fbd18e680b1071b7b6d50fb
GaneshDevkar <86193892+GaneshDevkar@users.noreply.github.com>
2023-09-06 19:48:23 +0530

    two card slider issue resolved in iteneries page.

--- public/assets/javascripts/card-slider02.js
+++ public/assets/javascripts/card-slider02.js
@@ -1,70 +1,72 @@
-let carousel01 = document.querySelector(".carousel01"),
-        firstImg = carousel01.querySelectorAll(".card_slider_inr")[0],
-        arrowIcons = document.querySelectorAll(".card_slider_row i");
+setTimeout(() => {
+    let carousel01 = document.querySelector(".carousel01"),
+        firstImg1 = carousel01.querySelectorAll(".card_slider_inr01")[0],
+        arrowIcons1 = document.querySelectorAll(".card_slider_row01 i");
 
-        let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+    let isdragStart1 = false, isdragging1 = false, prevPageX1, prevScrollLeft1, positionDiff1;
 
-        let showHideIcons = () => {
-            let scrollWidth = carousel01.scrollWidth - carousel01.clientWidth;
-            arrowIcons[0].style.display = carousel01.scrollLeft == 0 ? "none" : "block";
-            arrowIcons[1].style.display = carousel01.scrollLeft == scrollWidth ? "none" : "block";
-        }
+    let showHideIcons1 = () => {
+        let scrollWidth = carousel01.scrollWidth - carousel01.clientWidth;
+        arrowIcons1[0].style.display = carousel01.scrollLeft == 0 ? "none" : "block";
+        arrowIcons1[1].style.display = carousel01.scrollLeft == scrollWidth ? "none" : "block";
+    }
 
-        arrowIcons.forEach(icon => {
-            icon.addEventListener("click", () => {
-                let firstImgWidth = firstImg.clientWidth;
-                carousel01.scrollLeft += icon.id == "leftt" ? -firstImgWidth : firstImgWidth;
-                setTimeout(() => showHideIcons(), 60); 
-            });
+    arrowIcons1.forEach(icon => {
+        icon.addEventListener("click", () => {
+            let firstImgWidth = firstImg1.clientWidth;
+            carousel01.scrollLeft += icon.id == "leftt" ? -firstImgWidth : firstImgWidth;
+            setTimeout(() => showHideIcons1(), 60);
         });
+    });
 
-        let autoSlide = () => {
-            
-            if(carousel01.scrollLeft - (carousel01.scrollWidth - carousel01.clientWidth) > -1 || carousel01.scrollLeft <= 0) return;
+    let autoSlide1 = () => {
 
-            positionDiff = Math.abs(positionDiff); 
-            let firstImgWidth = firstImg.clientWidth;
-        
-            let valDifference = firstImgWidth - positionDiff;
+        if (carousel01.scrollLeft - (carousel01.scrollWidth - carousel01.clientWidth) > -1 || carousel01.scrollLeft <= 0) return;
 
-            if(carousel01.scrollLeft > prevScrollLeft) { 
-                return carousel01.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-            }
-        
-            carousel01.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+        positionDiff1 = Math.abs(positionDiff1);
+        let firstImgWidth = firstImg1.clientWidth;
+
+        let valDifference = firstImgWidth - positionDiff1;
+
+        if (carousel01.scrollLeft > prevScrollLeft1) {
+            return carousel01.scrollLeft += positionDiff1 > firstImgWidth / 3 ? valDifference : -positionDiff1;
         }
 
-        let dragStart = (e) => {
-            isDragStart = true;
-            prevPageX = e.pageX || e.touches[0].pageX;
-            prevScrollLeft = carousel01.scrollLeft;
-        }
+        carousel01.scrollLeft -= positionDiff1 > firstImgWidth / 3 ? valDifference : -positionDiff1;
+    }
 
-        let dragging = (e) => {
-            if(!isDragStart) return;
-            e.preventDefault();
-            isDragging = true;
-            carousel01.classList.add("dragging");
-            positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-            carousel01.scrollLeft = prevScrollLeft - positionDiff;
-            showHideIcons();
-        }
+    let dragStart1 = (e) => {
+        isdragStart1 = true;
+        prevPageX1 = e.pageX || e.touches[0].pageX;
+        prevScrollLeft1 = carousel01.scrollLeft;
+    }
 
-        let dragStop = () => {
-            isDragStart = false;
-            carousel01.classList.remove("dragging");
+    let dragging1 = (e) => {
+        if (!isdragStart1) return;
+        e.preventDefault();
+        isdragging1 = true;
+        carousel01.classList.add("dragging");
+        positionDiff1 = (e.pageX || e.touches[0].pageX) - prevPageX1;
+        carousel01.scrollLeft = prevScrollLeft1 - positionDiff1;
+        showHideIcons1();
+    }
 
-            if(!isDragging) return;
-            isDragging = false;
-            autoSlide();
-        }
+    let dragStop1 = () => {
+        isdragStart1 = false;
+        carousel01.classList.remove("dragging");
 
-        carousel01.addEventListener("mousedown", dragStart);
-        carousel01.addEventListener("touchstart", dragStart);
+        if (!isdragging1) return;
+        isdragging1 = false;
+        autoSlide1();
+    }
 
-        document.addEventListener("mousemove", dragging);
-        carousel01.addEventListener("touchmove", dragging);
+    carousel01.addEventListener("mousedown", dragStart1);
+    carousel01.addEventListener("touchstart", dragStart1);
 
-        document.addEventListener("mouseup", dragStop);
-        carousel01.addEventListener("touchend", dragStop);
+    document.addEventListener("mousemove", dragging1);
+    carousel01.addEventListener("touchmove", dragging1);
 
+    document.addEventListener("mouseup", dragStop1);
+    carousel01.addEventListener("touchend", dragStop1);
+
+}, 1000);
