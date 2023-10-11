7ceaf2eebfdfa8ffad77ce01b09fde7e5657ed2e
shashikant@isgesolutions.com <shashikant@isgesolutions.com>
2023-10-11 15:54:47 -0400

    worked on the contact us form

--- services/contactus.service.js
+++ services/contactus.service.js
@@ -5,17 +5,18 @@ const { publicRuntimeConfig } = getConfig();
 const baseUrlEnquiries = `${publicRuntimeConfig.apiUrl}/api/enquiries`;
 const baseUrlBrochure = `${publicRuntimeConfig.apiUrl}/api/brochure-requests`;;
 
+const contactusEmailUrl = `${publicRuntimeConfig.apiUrl}/api/api_four`;
+
 export const contactusService = {
     makeanenquiry,
     brochureRequest
 };
 
 function makeanenquiry(contactusData) {
-    return fetchWrapper.post(`${baseUrlEnquiries}`, contactusData);
+    const currentUrl = window?.location?.origin + '/api/api_four';
+    return fetchWrapper.post(`${currentUrl}`, contactusData);
 }
 
 function brochureRequest(brochureData) {
     return fetchWrapper.post(`${baseUrlBrochure}`, brochureData);
 }
-
-
