7092f34edd2f20d50160e8bcb21de12193cf9bba
shashikant@isgesolutions.com <shashikant@isgesolutions.com>
2023-10-17 17:06:40 -0400

    worked on the http loader

---
+++ components/MyLoader.jsx
@@ -0,0 +1,13 @@
+// Loader.js
+import React from 'react';
+
+function MyLoader() {
+    return (
+        <div className="full_loader_parnt_blk loader_parnt_blk" style={{ display: `block !important` }}>
+            <div class="loader-circle-2">
+            </div>
+        </div>
+    );
+}
+
+export default MyLoader;
