cadb87a1fd50025c97ae62b65530d37943324511
shashikant@isgesolutions.com <shashikant@isgesolutions.com>
2023-10-18 22:02:20 -0400

    worked on the facebook pixel

--- components/facebookPixel.js
+++ components/facebookPixel.js
@@ -1,9 +1,9 @@
-// facebookPixel.js
-import ReactPixel from 'react-facebook-pixel';
+// // facebookPixel.js
+// import ReactPixel from 'react-facebook-pixel';
 
-ReactPixel.init('YOUR_PIXEL_ID', {
-  autoConfig: true, // Automatic configuration based on Pixel ID
-  debug: false, // Set to true for development/debugging
-});
+// ReactPixel.init('YOUR_PIXEL_ID', {
+//   autoConfig: true, // Automatic configuration based on Pixel ID
+//   debug: false, // Set to true for development/debugging
+// });
 
-export default ReactPixel;
+// export default ReactPixel;
