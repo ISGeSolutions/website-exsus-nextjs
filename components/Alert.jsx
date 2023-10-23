31797923ce0c1a06bc81701bfc6795a51db82b55
shashikant@isgesolutions.com <shashikant@isgesolutions.com>
2023-10-23 16:27:57 -0400

    worked on the Alert

--- components/Alert.jsx
+++ components/Alert.jsx
@@ -17,7 +17,7 @@ function Alert({ message, type, onClose }) {
         // $(".succss_msg_parnt").hide();
 
         if (!message && !type) {
-            $(".succss_msg_parnt").hide();
+            // $(".succss_msg_parnt").hide();
         }
 
     }, [message, type, isInitialLoad]);
@@ -32,9 +32,9 @@ function Alert({ message, type, onClose }) {
                 <div className="succss_msg_inr">
                     <div className="modal-header">
                         <div className="succss_msg_cross">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            <svg onClick={onCloseFun} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                 className="bi bi-x" viewBox="0 0 16 16">
-                                <path
+                                <path className="btn-link"
                                     d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
                             </svg>
                         </div>
