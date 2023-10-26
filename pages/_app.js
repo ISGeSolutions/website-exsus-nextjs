966b3e7a5c692b07db1b25456d9299bd88f2ecb0
shashikant@isgesolutions.com <shashikant@isgesolutions.com>
2023-10-26 17:03:22 -0400

    worked on the facebook pixel code

--- pages/_app.js
+++ pages/_app.js
@@ -71,7 +71,7 @@ function FacebookPixel() {
     import("react-facebook-pixel")
       .then((x) => x.default)
       .then((ReactPixel) => {
-        ReactPixel.init("YOUR_PIXEL_ID");
+        ReactPixel.init("YOUR_PIXEL_ID"); // 1613634385322868
         ReactPixel.pageView();
         Router.events.on("routeChangeComplete", () => {
           ReactPixel.pageView();
@@ -192,7 +192,6 @@ function App({ Component, pageProps }) {
       </I18nextProvider>
       {/* document.getElementById('root')
                 ); */}
-
       <Helmet>
         <link rel="canonical" href="https://website-exsus-nextjs.vercel.app" />
       </Helmet>
@@ -235,6 +234,23 @@ function App({ Component, pageProps }) {
       {/* <script type="text/javascript" src="/assets/javascripts/navigation.js"></script> */}
 
       {/* credits */}
+
+      {/* Facebook Pixel Code */}
+      <script>
+        {/* !function (f, b, e, v, n, t, s) {
+      if (f.fbq) return; n = f.fbq = function () {
+          n.callMethod ?
+            n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+        }; if (!f._fbq) f._fbq = n;
+        n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = []; t = b.createElement(e); t.async = !0;
+        t.src = v; s = b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t, s)
+    }(window,
+        document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1613634385322868'); // Insert your pixel ID here.
+        fbq('track', 'PageView'); */}
+      </script>
+      {/* Facebook Pixel Code */}
+
     </Layout>
   ) : (
     <Component {...pageProps} />
