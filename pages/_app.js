633bb7c66d2cce56b990a78a506ade3082f10fa3
shashikant@isgesolutions.com <shashikant@isgesolutions.com>
2023-10-30 12:01:58 -0400

    worked on the facebook pixel code

--- pages/_app.js
+++ pages/_app.js
@@ -233,28 +233,9 @@ function App({ Component, pageProps }) {
         src="/assets/javascripts/bootstrap-select.min.js"
       ></script>
       {/* <script type="text/javascript" src="/assets/javascripts/navigation.js"></script> */}
-
       {/* credits */}
 
-      {/* Facebook Pixel Code */}
-
-      {/* Your page content */}
       <Script src="/assets/javascripts/facebook-pixel.js" strategy="beforeInteractive" />
-
-      <script>
-        {/* !function (f, b, e, v, n, t, s) {
-      if (f.fbq) return; n = f.fbq = function () {
-          n.callMethod ?
-            n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-        }; if (!f._fbq) f._fbq = n;
-        n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = []; t = b.createElement(e); t.async = !0;
-        t.src = v; s = b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t, s)
-    }(window,
-        document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '1613634385322868'); // Insert your pixel ID here.
-        fbq('track', 'PageView'); */}
-      </script>
-      {/* Facebook Pixel Code */}
 
     </Layout>
   ) : (
