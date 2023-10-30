ea2e1db002250c552b7412638b7f728d95314449
shashikant@isgesolutions.com <shashikant@isgesolutions.com>
2023-10-30 11:46:50 -0400

    worked on the facebook pixel code

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import React, { useState, useEffect } from "react";
 import { Router, useRouter } from "next/router";
 // import React from 'react';
-import { initPixel } from 'react-facebook-pixel';
+
 // import { NavLink } from './../components/NavLink';
 import { Layout } from "./../components/common";
 
@@ -66,20 +66,20 @@ i18n.use(LanguageDetector).init({
   },
 });
 
-// function FacebookPixel() {
-//   React.useEffect(() => {
-//     import("react-facebook-pixel")
-//       .then((x) => x.default)
-//       .then((ReactPixel) => {
-//         ReactPixel.init("YOUR_PIXEL_ID"); // 1613634385322868
-//         ReactPixel.pageView();
-//         Router.events.on("routeChangeComplete", () => {
-//           ReactPixel.pageView();
-//         });
-//       });
-//   });
-//   return null;
-// }
+function FacebookPixel() {
+  React.useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init("YOUR_PIXEL_ID"); // 1613634385322868
+        ReactPixel.pageView();
+        Router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });
+  });
+  return null;
+}
 
 export default App;
 
@@ -137,9 +137,6 @@ function App({ Component, pageProps }) {
     //     console.log('x', x);
     // });
 
-    // Initialize the Facebook Pixel with your Pixel ID
-    initPixel('G-2H6GP9JWWY');
-
     // unsubscribe from events in useEffect return function
     return () => {
       router.events.off("routeChangeStart", hideContent);
@@ -147,9 +144,6 @@ function App({ Component, pageProps }) {
     };
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-
-
-
   }, []);
 
   function authCheck(url) {
@@ -237,7 +231,6 @@ function App({ Component, pageProps }) {
         type="text/javascript"
         src="/assets/javascripts/bootstrap-select.min.js"
       ></script>
-
       {/* <script type="text/javascript" src="/assets/javascripts/navigation.js"></script> */}
 
       {/* credits */}
@@ -257,6 +250,7 @@ function App({ Component, pageProps }) {
         fbq('track', 'PageView'); */}
       </script>
       {/* Facebook Pixel Code */}
+
     </Layout>
   ) : (
     <Component {...pageProps} />
