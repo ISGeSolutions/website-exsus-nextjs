06dd05357fa19aae6542bb957b15310f00f2d3aa
GaneshDevkar <86193892+GaneshDevkar@users.noreply.github.com>
2024-02-12 13:48:21 +0530

    making_image_sliders_and_forms_code_common

--- pages/index.jsx
+++ pages/index.jsx
@@ -505,32 +505,6 @@ function Index() {
       ) : (
         <div>
           <section className="banner_blk_row">
-            {/* <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-              <ol className="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-              </ol>
-              <div className="carousel-inner">
-                <div className="carousel-item active">
-                  <p>Test1</p>
-                </div>
-                <div className="carousel-item">
-                  <p>Test2</p>
-                </div>
-                <div className="carousel-item">
-                  <p>Test3</p>
-                </div>
-              </div>
-              <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="sr-only">Previous</span>
-              </a>
-              <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="sr-only">Next</span>
-              </a>
-            </div> */}
             {backgroundImage ? (
               <div
                 id="carouselExampleInterval"
