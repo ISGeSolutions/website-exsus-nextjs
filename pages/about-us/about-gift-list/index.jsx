49daf76de1ff2f43d2aa62a83a151558bdc4401d
GaneshDevkar <86193892+GaneshDevkar@users.noreply.github.com>
2024-02-13 11:43:27 +0530

    worked on special offer api call

--- pages/about-us/about-gift-list/index.jsx
+++ pages/about-us/about-gift-list/index.jsx
@@ -43,6 +43,19 @@ function Index() {
     event.preventDefault();
   };
 
+  const disableAnchorTags = (htmlString) => {
+    const tempElement = document.createElement('div');
+    tempElement.innerHTML = htmlString;
+
+    // Disable anchor tags
+    const anchorTags = tempElement.querySelectorAll('a');
+    anchorTags.forEach(anchor => {
+      anchor.removeAttribute('href');
+    });
+
+    return tempElement.innerHTML;
+  };
+
   const websiteContentCheck = (matches, modifiedString) => {
     destinationService
       .getDictionaryDetails(matches, region)
@@ -90,7 +103,7 @@ function Index() {
             } else if (element?.attributes?.content_name == "MetaDescription") {
               setMetaDescription(element?.attributes?.content_value);
             } else if (element?.attributes?.content_name == "Long_Text") {
-              modifiedString = element?.attributes?.content_value;
+              modifiedString = disableAnchorTags(element?.attributes?.content_value);
             } else if (element?.attributes?.content_name == "Right_Header") {
               setRightHeader(element?.attributes?.content_value);
             } else if (element?.attributes?.content_name == "Right_Corner") {
