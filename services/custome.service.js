8c47c73ab93492601ad3610d080b5040a1a174a4
Chaitanya Narayankar <chaitanya@isgesolutions.com>
2024-02-26 20:13:17 +0530

    testing_point_resolve_two

---
+++ services/custome.service.js
@@ -0,0 +1,20 @@
+import { BehaviorSubject } from "rxjs";
+import getConfig from "next/config";
+import Router from "next/router";
+import { fetchWrapper } from "helpers";
+const { publicRuntimeConfig } = getConfig();
+
+export const customeService = {
+  getTravelInformation,
+  getAdvanceSearch,
+};
+
+function getTravelInformation() {
+  const travelInformationUrl = `${publicRuntimeConfig.apiUrl}/api/custom-pages?populate[0]=custom_page_images&populate[1]=custom_page_contents&filters[page_code][$eq]=Useful-Links`;
+  return fetchWrapper.get(travelInformationUrl);
+}
+
+function getAdvanceSearch() {
+  const travelInformationUrl = `${publicRuntimeConfig.apiUrl}/api/custom-pages?populate[0]=custom_page_images&populate[1]=custom_page_contents&filters[page_code][$eq]=Advanced-search`;
+  return fetchWrapper.get(travelInformationUrl);
+}
